3e01bbb810dd2e03d34556814cb14b4bfdc39983
vinay kumar <madasuvinaykumar123@gmail.com>
2024-11-24 14:57:57 +0530

    Initial commit with project setup

--- src/Pagination.js
+++ src/Pagination.js
@@ -3,11 +3,11 @@ import React from "react";
 const Pagination = ({ currentPage, totalPages, onPrevious, onNext }) => {
   return (
     <div className="pagination">
-      <button onClick={onPrevious} disabled={currentPage === 1}>
+      <button onClick={onPrevious} /*disabled={currentPage === 1}*/>
         Previous
       </button>
       <span>{currentPage}</span>
-      <button onClick={onNext} disabled={currentPage === totalPages}>
+      <button onClick={onNext} /*disabled={currentPage === totalPages}*/>
         Next
       </button>
     </div>
